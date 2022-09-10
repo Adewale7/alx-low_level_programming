cf7649f1a443fd1e6742b15477cb00784c94f306
Adewale7 <Adewaledavid777@gmail.com>
2022-09-10 00:20:59 -0700

    task10

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ n = rand() - RAND_MAX / 2;
 L = n % 10;
 if (L > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, L)
+printf("Last digit of %d is %d and is greater than 5\n", n, L);
 }
 else if (L == 0)
 {
